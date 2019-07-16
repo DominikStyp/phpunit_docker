8331430ab0ef5b6381ad25709624cb0535384d5a
DominikStyp <dominik.stypula1@gmail.com>
2019-07-16 06:39:07 +0200

    additional option -c with possibility to copy vendor or not

--- php7-testing-phpunit/initialScript.sh
+++ php7-testing-phpunit/initialScript.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+copyVendor=0
+usage()
+{
+    echo "usage: docker-compose run phpunit [-c | --copy-vendor (script will copy vendor directory from container to your project] "
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -c | --copy-vendor )    shift
+                                copyVendor=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
 ls -lah /app/tests
-cp -R /tmp/vendor/* /tmp/vendor_mapped
+if [[ $copyVendor -eq 1 ]]; then
+    echo "Copying files from container to your ./vendor";
+    cp -R /tmp/vendor/* /tmp/vendor_mapped;
+fi
 phpunit /app/tests
