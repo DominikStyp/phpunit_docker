fb112d21b7b2331f082db5d8376c279c5622889f
DominikStyp <dominik.stypula1@gmail.com>
2019-07-16 14:34:31 +0200

    initialScript.sh refactoring

--- php7-testing-phpunit/initialScript.sh
+++ php7-testing-phpunit/initialScript.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-copyVendor=0
 usage()
 {
-    echo "usage: docker-compose run phpunit [-c | --copy-vendor (script will copy vendor directory from container to your project] "
+    echo "usage: docker-compose run phpunit [options] "
+    echo "-c | --copy-vendor - script will ONLY copy vendor directory from container to your project"
+    echo "                     which may be useful for your IDE to autocomplete PHPUnit classes etc."
+    exit
 }
-
+copyVendor()
+{
+    echo "Copying files from container to your ./vendor";
+    cp -R /tmp/vendor/* /tmp/vendor_mapped;
+    exit;
+}
 while [ "$1" != "" ]; do
     case $1 in
         -c | --copy-vendor )    shift
-                                copyVendor=1
+                                copyVendor
                                 ;;
         -h | --help )           usage
-                                exit
                                 ;;
         * )                     usage
-                                exit 1
     esac
     shift
 done
 
-ls -lah /app/tests
-if [[ $copyVendor -eq 1 ]]; then
-    echo "Copying files from container to your ./vendor";
-    cp -R /tmp/vendor/* /tmp/vendor_mapped;
-fi
 phpunit /app/tests
